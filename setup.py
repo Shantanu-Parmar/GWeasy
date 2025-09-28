ba3076a781028f9c63d3d5b23694c828f6cd0aaf
Shantanu Parmar <113082290+Shantanu-Parmar@users.noreply.github.com>
2025-09-29 00:44:47 +0530

    Bump version from 0.1.0 to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gweasy",
-    version="0.1.0",
+    version="0.2.0",
     description="A user-friendly GUI for gravitational wave data analysis",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
